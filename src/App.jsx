4e5b165be46a2e417f339777a336be1b04b7658b
amamam1231 <securevpntg@gmail.com>
2026-03-02 00:33:42 +0100

    Edit site 414

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs))
 }
 
 function App() {
